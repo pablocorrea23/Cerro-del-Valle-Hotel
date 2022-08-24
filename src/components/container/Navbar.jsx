211f3d0765ac80e1d6cc6d7c77cd41f210de07ab
pablocorrea23 <pablocorrea34@gmail.com>
2022-08-24 11:04:51 -0300

    cambios

---
+++ src/components/container/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const Navbar = () => {
+    return (
+        <header className="header">
+            {/* <h1>Logo</h1> */}
+            <nav>
+                <ul>
+                    <li>Home</li>
+                    <li>Servicios</li>
+                    <li>Galeria de fotos</li>
+                    <li>Contacto</li>
+                </ul>
+            </nav>
+        </header>
+    )
+}
